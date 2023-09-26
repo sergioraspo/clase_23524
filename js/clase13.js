6cb3933c9d75ccbaed7753d2e08f4b8c7ffcd009
sergioraspo <sergioraspo@gmail.com>
2023-09-26 18:42:37 -0300

    trabajando con js

--- js/clase13.js
+++ js/clase13.js
@@ -1,1 +1,37 @@
 console.log('Hola Mundo JS, por consola')
+
+console.error('Esto es un error')
+console.warn('Esta sucediendo algo inesperado a mi codigo')
+
+var movie;
+
+movie = "El Padrino"; 
+movie = "El Rey Leon";
+
+/* Ejemplo que comienza en el minuto 30:00 para el desarrollo de variables */
+
+console.log(movie)
+
+/* ver funcionamiento de la funcion typeof a partir del 39:00*/
+
+console.log(typeof(movie))
+
+
+var ratting = 8;
+console.log(typeof(ratting)); 
+
+var ratting2 = 9.2;
+console.log(typeof(ratting2));
+var ratting3 = 15.1;
+var IsNew = false;
+console.log(typeof(IsNew))
+/* para valores undefined variables*/
+var movie2 = null;
+console.log(typeof(movie2));
+
+const CURRENT_YEAR = 2023;
+
+// OPERADORES ARITMETICOS
+
+var average = (ratting + ratting2)/2
+console.log('El promedio es: ', average);
