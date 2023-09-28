2ff1895a038aa30b39ccfd17d5390ba8e9bc84ae
sergioraspo <sergioraspo@gmail.com>
2023-09-28 07:53:05 -0300

    agregando clase13

--- js/clase13.js
+++ js/clase13.js
@@ -35,3 +35,12 @@ const CURRENT_YEAR = 2023;
 
 var average = (ratting + ratting2)/2
 console.log('El promedio es: ', average);
+
+
+var age = 10; 
+
+if(age>= 13){
+    console.log('Puedes ver la pelicula, edad adecuada')
+}else{
+    console.warn('No puedes ver la pelicula, edad inadecuada')
+}
